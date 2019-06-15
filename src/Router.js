c8863aec82df8244b5758309670c69c7f4de462c
MAJU STUDENT <majustudent@jinnah.edu>
2019-06-15 16:55:59 +0500

    code pushed

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { Router, Scene } from 'react-native-router-flux'
 import Home from './Home.js'
 import objectives from './Components/objectives'
 import experiance&education from './Components/objectives'
-
+import LoginForm from "./components/LoginForm";
 
 const Routes = () => (
    <Router>
@@ -12,5 +12,17 @@ const Routes = () => (
          <Scene key = "about" component = {About} title = "objectives" />
          <Scene key = "about" component = {About} title = "experiation&education" />      </Scene>
    </Router>
+   const RouterComponent = () => {
+      return (
+        <Router>
+          <Scene key="root" hideNavBar>
+            <Scene key="auth">
+              <Scene
+                key="login"
+                component={LoginForm}
+                title="Please Login"
+                initial
+              />
+            </Scene>
 )
 export default Routes
